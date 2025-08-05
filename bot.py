1239ef6a20d86f0216a93de6dcc3a4ae4ed992b0
Fazzer99 <faissalnurin@gmail.com>
2025-08-05 23:22:07 +0200

    XXX

--- bot.py
+++ bot.py
@@ -691,7 +691,7 @@ async def on_guild_join(guild: discord.Guild):
         "• `!setup vc_override`\n"
         "  – Danach fragt der Bot nacheinander:\n"
         "    1. **Override-Rollen** erwähnen (z.B. `@Admin @Moderator`)\n"
-        "    2. **Ziel-Rollen** erwähnen, die bei Beitritt einer Override-Rolle automatisch Zugriff auf gesperrte Voice-Channels erhalten\n"
+        "    2. **Ziel-Rollen** erwähnen, die bei Beitritt einer Override-Rolle automatisch Zugriff auf gesperrte Voice-Channels erhalten\n\n"
         "• `!disable <module>`\n"
         "  – Deaktiviert ein eingerichtetes Modul und entfernt dazu alle gespeicherten Einstellungen:\n"
         "    – `!disable welcome` oder `!disable leave`\n"
